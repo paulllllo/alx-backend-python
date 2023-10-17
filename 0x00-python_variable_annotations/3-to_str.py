49c57bf8dbe9e63d1f2785f202465cedbcbb5ec0
paul <paulkelechi11@gmail.com>
2023-10-17 01:25:01 -0700

    Added a function that returns the string of a float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" A module that defines basic function type annotations """
+
+
+def to_str(n: float) -> str:
+    """A function that returns the string of a float"""
+    return str(n)
