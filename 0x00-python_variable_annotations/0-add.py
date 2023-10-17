03921ff3925531b9b9a5a86ad73766352333853b
paul <paulkelechi11@gmail.com>
2023-10-17 00:17:49 -0700

    Added a module that defines a basic annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+""" A module that defines basic python annotations """
+
+def add(a: float, b: float) -> float:
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+A dir created for alx project python variable annotations
